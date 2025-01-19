099792752e508a9eacaa7d3feb7dc73912302f7a
Ara Winters <ara.winters@gmail.com>
2025-01-19 11:02:29 -0800

    helloworld example

---
+++ examples/helloworld/helloworld.ts
@@ -0,0 +1,12 @@
+import { SzAbstractFactory, SzAbstractFactoryOptions } from '@senzing/sz-sdk-typescript-grpc';
+
+const szParamFactory = new SzAbstractFactory(`0.0.0.0:8261`);
+const szProduct      = szParamFactory.createProduct();
+
+szProduct.getVersion().
+    then((result)=>{
+        console.log("RESPONSE:\n\r", result);
+    }).
+    catch((err)=>{
+        console.error(err);
+    });
