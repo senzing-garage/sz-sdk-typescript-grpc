056c1b9b5e2c6619b17823daf94208a86b998ff5
Ara Winters <ara.winters@gmail.com>
2025-02-05 21:08:45 -0800

    #10 testing framework, gha, and basic tests

---
+++ test/SzEngine.spec.ts
@@ -0,0 +1,17 @@
+import { SzGrpcEngine, SzGrpcEnvironment } from "../dist/@senzing/sz-sdk-typescript-grpc";
+
+describe('SzEngine Tests', () => {
+    const szEnvironment  = new SzGrpcEnvironment({isTestEnvironment: true});
+    let szEngine: SzGrpcEngine;
+
+    beforeAll(() => {
+        szEngine = szEnvironment.getEngine();
+    });
+
+    it("Can create SzEngine", () => {
+        expect(szEngine).toHaveProperty('productId');
+    });
+    it("Has correct product id", () => {
+        expect(szEngine.productId).toBe('5053');
+    });
+})
---
+++ test/SzEnvironment.spec.ts
@@ -0,0 +1,24 @@
+import { SzGrpcConfig, SzGrpcEnvironment } from "../dist/@senzing/sz-sdk-typescript-grpc";
+
+describe('SzEnvironment Tests', () => {
+    const szEnvironment  = new SzGrpcEnvironment({isTestEnvironment: true});
+
+    it("Can create SzEnvironment", () => {
+        expect(szEnvironment).toBeInstanceOf(SzGrpcEnvironment);
+    });
+    it("Has getConfig method", () => {
+        expect(typeof szEnvironment.getConfig).toBe('function');
+    });
+    it("Has getConfigManager method", () => {
+        expect(typeof szEnvironment.getConfigManager).toBe('function');
+    });
+    it("Has getDiagnostic method", () => {
+        expect(typeof szEnvironment.getDiagnostic).toBe('function');
+    });
+    it("Has getEngine method", () => {
+        expect(typeof szEnvironment.getEngine).toBe('function');
+    });
+    it("Has getProduct method", () => {
+        expect(typeof szEnvironment.getProduct).toBe('function');
+    });
+})
